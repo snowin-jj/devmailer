bca3f927507e60ed205ca5e865c9697cf3668cb9
devsnowin <snowin.developer@gmail.com>
2023-04-30 15:42:24 +0530

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { SessionProvider } from 'next-auth/react';
+import { Analytics } from '@vercel/analytics/react';
 import { ChakraProvider } from '@chakra-ui/react';
+
 import { theme } from '../theme';
 import Layout from '@/layout/Layout';
 
@@ -8,16 +10,19 @@ export default function App({
 	pageProps: { session, ...pageProps },
 }) {
 	return (
-		<ChakraProvider theme={theme}>
-			<SessionProvider session={session}>
-				{Component.authPage ? (
-					<Component {...pageProps} />
-				) : (
-					<Layout>
+		<>
+			<ChakraProvider theme={theme}>
+				<SessionProvider session={session}>
+					{Component.authPage ? (
 						<Component {...pageProps} />
-					</Layout>
-				)}
-			</SessionProvider>
-		</ChakraProvider>
+					) : (
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					)}
+				</SessionProvider>
+			</ChakraProvider>
+			<Analytics />
+		</>
 	);
 }
